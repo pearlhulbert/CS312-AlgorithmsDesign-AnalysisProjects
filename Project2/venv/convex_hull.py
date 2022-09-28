d8519cb86d1a7f06bcb6a968d154765bbc9f26c7
clamguts <pearl.k.hulbert@gmail.com>
2022-09-28 15:57:23 -0600

    finished project 2

--- Project2/venv/convex_hull.py
+++ Project2/venv/convex_hull.py
@@ -1,24 +1,24 @@
 from linked_list import Node, CircleDoubleLinkedList
 from which_pyqt import PYQT_VER
+
 if PYQT_VER == 'PYQT5':
-	from PyQt5.QtCore import QLineF, QPointF, QObject
+    from PyQt5.QtCore import QLineF, QPointF, QObject
 elif PYQT_VER == 'PYQT4':
-	from PyQt4.QtCore import QLineF, QPointF, QObject
+    from PyQt4.QtCore import QLineF, QPointF, QObject
 else:
-	raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
-
-
+    raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
 
 import time
 
 # Some global color constants that might be useful
-RED = (255,0,0)
-GREEN = (0,255,0)
-BLUE = (0,0,255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
 # Global variable that controls the speed of the recursion automation, in seconds
 #
 PAUSE = 0.25
+
 
 #
 # This is the class you have to complete.
@@ -26,222 +26,199 @@ PAUSE = 0.25
 
 class Hull:
 
-	def __init__(self, cirle_linked_list):
-		self.list = cirle_linked_list
+    def __init__(self, circle_linked_list):
+        self.list = circle_linked_list
 
-	def size(self):
-		return self.list.get_size()
+    def size(self):
+        return self.list.get_size()
 
-	def right_side(self):
-		return self.list.tail
+    def right_side(self):
+        return self.list.tail
 
-	def left_side(self):
-		return self.list.head
+    def left_side(self):
+        return self.list.head
 
-
-
+#This is O(1) or constant
 def calculate_slope(left_node, right_node):
-	x_1 = left_node.value.x()
-	y_2 = left_node.value.y()
+    x_1 = left_node.value.x()
+    y_1 = left_node.value.y()
 
-	x_2 = right_node.value.x()
-	y_2 = right_node.value.y()
+    x_2 = right_node.value.x()
+    y_2 = right_node.value.y()
 
-	slope = (y_2 - y_1) / (x_2 - x_1)
+    slope = (y_2 - y_1) / (x_2 - x_1)
 
-	return slope
+    return slope
 
-def list_in_half(circle_linked_list):
-	list_size = circle_linked_list.get_size()
-	middle = list_size // 2
-	first_half = CircleDoubleLinkedList()
-	last_half = CircleDoubleLinkedList()
-
-	current = circle_linked_list.get_head()
-
-	for i in range(list_size):
-		if i < middle:
-			first_half.insert_tail(current.value)
-		else:
-			last_half.insert_tail(current.value)
-		current = current.next
-
-	return first_half, last_half
 
 def upper_tangent(left_right_most, right_left_most, left_half, right_half):
-	best_left_guess = left_right_most
-	best_right_guess = right_left_most
+    left_guess = left_right_most
+    right_guess = right_left_most
 
-	curr_left = left_right_most
-	curr_right = right_left_most
+    curr_left = left_right_most
+    curr_right = right_left_most
 
-	best_guess_slope = calculate_slope(curr_left, curr_right)
+    best_guess_slope = calculate_slope(curr_left, curr_right)
 
-	found = False
+    found = False
 
-	while not found:
-		negative = False
-		positive = False
-		for i in range(left_half.get_size()):
-			curr_left = curr_left.prev
-			new_slope = calculate_slope(curr_left, best_right_guess)
-			if new_slope < best_guess_slope:
-				best_left_guess = curr_left
-				negative = True
-				best_guess_slope = new_slope
-		for i in range(right_half.get_size()):
-			curr_node = curr_right.next
-			new_slope = calculate_slope(best_left_guess, curr_right)
-			if new_slope > best_guess_slope:
-				best_right_guess = curr_node
-				positive = True
-				best_guess_slope = new_slope
-		if not negative and not positive:
-			found = True
+    while not found:
+        negative = False
+        positive = False
+        for i in range(left_half.size()):
+            curr_left = curr_left.prev
+            new_slope = calculate_slope(curr_left, right_guess)
+            if new_slope < best_guess_slope:
+                left_guess = curr_left
+                negative = True
+                best_guess_slope = new_slope
+        for i in range(right_half.size()):
+            curr_right = curr_right.next
+            new_slope = calculate_slope(left_guess, curr_right)
+            if new_slope > best_guess_slope:
+                right_guess = curr_right
+                positive = True
+                best_guess_slope = new_slope
+        if not negative and not positive:
+            found = True
 
-	#This is the value we actually need for combining the hulls
-	return best_right_guess
+    # This is the value we actually need for combining the hulls
+    return left_guess, right_guess
 
 
 def lower_tangent(left_right_most, right_left_most, left_half, right_half):
-	best_left_guess = left_right_most
-	best_right_guess = right_left_most
+    left_guess = left_right_most
+    right_guess = right_left_most
 
-	curr_left = left_right_most
-	curr_right = right_left_most
+    curr_left = left_right_most
+    curr_right = right_left_most
 
-	best_guess_slope = calculate_slope(curr_left, curr_right)
+    best_guess_slope = calculate_slope(curr_left, curr_right)
 
-	found = False
+    found = False
 
-	while not found:
-		negative = False
-		positive = False
-		for i in range(left_half.get_size()):
-			curr_left = curr_left.next
-			new_slope = calculate_slope(curr_left, best_right_guess)
-			if new_slope > best_guess_slope:
-				best_left_guess = curr_left
-				negative = True
-				best_guess_slope = new_slope
-		for i in range(0, right_half.get_size()):
-			curr_right = curr_right.prev
-			new_slope = calculate_slope(best_left_guess, curr_right)
-			if new_slope < best_guess_slope:
-				best_right_guess = curr_right
-				positive = True
-				best_guess_slope = new_slope
-		if not negative and not positive:
-			found = True
+    while not found:
+        negative = False
+        positive = False
+        for i in range(left_half.size()):
+            curr_left = curr_left.next
+            new_slope = calculate_slope(curr_left, right_guess)
+            if new_slope > best_guess_slope:
+                left_guess = curr_left
+                negative = True
+                best_guess_slope = new_slope
+        for i in range(right_half.size()):
+            curr_right = curr_right.prev
+            new_slope = calculate_slope(left_guess, curr_right)
+            if new_slope < best_guess_slope:
+                right_guess = curr_right
+                positive = True
+                best_guess_slope = new_slope
+        if not negative and not positive:
+            found = True
 
-	# This is the value we actually need for combining the hulls
-	return best_left_guess
+    # This is the value we actually need for combining the hulls
+    return left_guess, right_guess
 
-def combine_hulls(self, left_half, right_half):
+def combine_hulls(left_half, right_half):
+    left_right_most = left_half.right_side()
+    right_left_most = right_half.left_side()
 
-	left_right_most = left_half.right_side()
-	right_left_most = right_half.left_side()
+    upper_tangent_result = upper_tangent(left_right_most, right_left_most, left_half, right_half)
+    lower_tangent_result = lower_tangent(left_right_most, right_left_most, left_half, right_half)
 
-	upper_tangent_result = upper_tangent(left_right_most, right_left_most, left_half, right_half)
-	lower_tangent_result = lower_tangent(left_right_most, right_left_most, left_half, right_half)
+    left_upper_tangent, right_upper_tangent = upper_tangent_result
+    left_lower_tangent, right_lower_tangent = lower_tangent_result
 
-	best_left.next = upper_tangent_result
-	best_right.next = lower_tangent_result
+    left_upper_tangent.next = right_upper_tangent
+    right_upper_tangent.prev = left_upper_tangent
 
-	merge_list = CircleDoubleLinkedList()
+    right_lower_tangent.next = left_lower_tangent
+    left_lower_tangent.prev = right_lower_tangent
 
-	curr_node = upper_tangent_result
+    merge_list = CircleDoubleLinkedList()
 
-	while curr_node.next != upper_tangent_result:
-		merge_list.insert_tail(curr_node.value)
-		curr_node = curr_node.next
-	merge_list.insert_tail(curr_node.value)
+    merge_list.head = right_upper_tangent
+    merge_list.tail = left_upper_tangent
 
-	new_hull = Hull(merge_list)
-	return new_hull
+    new_hull = Hull(merge_list)
+    return new_hull
 
-def divide_and_conquer(self, points_list):
-	if points_list.get_size() <= 1:
-		curr_hull = Hull(points_list)
-		return curr_hull
-
-	left_half, right_half = list_in_half(points_list)
-
-	left_hull = self.divide_and_conquer(left_half)
-	right_hull = self.divide_and_conquer(right_half)
-
-	return self.combine_hulls(left_hull, right_hull)
-
-def make_hull(self, points):
-	linked_points = CircleDoubleLinkedList()
-
-	for elem in points:
-		linked_points.insert_tail(elem)
-
-	final_hull = self.divide_and_conquer(self, points)
-	return final_hull
 
 class ConvexHullSolver(QObject):
-# Class constructor
-	def __init__( self):
-		super().__init__()
-		self.pause = False
-		
-# Some helper methods that make calls to the GUI, allowing us to send updates
-# to be displayed.
+    # Class constructor
+    def __init__(self):
+        super().__init__()
+        self.pause = False
 
-	def showTangent(self, line, color):
-		self.view.addLines(line,color)
-		if self.pause:
-			time.sleep(PAUSE)
+    # Some helper methods that make calls to the GUI, allowing us to send updates
+    # to be displayed.
 
-	def eraseTangent(self, line):
-		self.view.clearLines(line)
+    def showTangent(self, line, color):
+        self.view.addLines(line, color)
+        if self.pause:
+            time.sleep(PAUSE)
 
-	def blinkTangent(self,line,color):
-		self.showTangent(line,color)
-		self.eraseTangent(line)
+    def eraseTangent(self, line):
+        self.view.clearLines(line)
 
-	def showHull(self, polygon, color):
-		self.view.addLines(polygon,color)
-		if self.pause:
-			time.sleep(PAUSE)
-		
-	def eraseHull(self,polygon):
-		self.view.clearLines(polygon)
-		
-	def showText(self,text):
-		self.view.displayStatusText(text)
-	
+    def blinkTangent(self, line, color):
+        self.showTangent(line, color)
+        self.eraseTangent(line)
 
-# This is the method that gets called by the GUI and actually executes
-# the finding of the hull
-	def compute_hull( self, points, pause, view):
-		self.pause = pause
-		self.view = view
-		assert( type(points) == list and type(points[0]) == QPointF )
+    def showHull(self, polygon, color):
+        self.view.addLines(polygon, color)
+        if self.pause:
+            time.sleep(PAUSE)
 
-		t1 = time.time()
-		points = sorted(points, key=lambda point: point.x())
-		t2 = time.time()
+    def eraseHull(self, polygon):
+        self.view.clearLines(polygon)
 
-		t3 = time.time()
-		# this is a dummy polygon of the first 3 unsorted points
-		final_hull = self.make_hull(points)
-		polygon = []
+    def showText(self, text):
+        self.view.displayStatusText(text)
 
-		#populate polygon list with values from linked list
-		linked_list_hull = final_hull.list
-		for val in linked_list_hull:
-			next = val.next
-			polygon.append(QLineF(val.value.x(), val.value.y(), next.value.x(), next.value.y()))
+    def divide_and_conquer(self, points):
+        if len(points) <= 1:
+            linked_list_points = CircleDoubleLinkedList()
+            linked_list_points.insert_tail(points[0])
+            curr_hull = Hull(linked_list_points)
+            return curr_hull
 
-		t4 = time.time()
+        middle_val = len(points) // 2
+        left_half = points[:middle_val]
+        right_half = points[middle_val:]
 
-		# when passing lines to the display, pass a list of QLineF objects.  Each QLineF
-		# object can be created with two QPointF objects corresponding to the endpoints
-		self.showHull(polygon,RED)
-		self.showText('Time Elapsed (Convex Hull): {:3.3f} sec'.format(t4-t3))
+        left_hull = self.divide_and_conquer(left_half)
+        right_hull = self.divide_and_conquer(right_half)
 
+        combined = combine_hulls(left_hull, right_hull)
+        return combined
 
+    # This is the method that gets called by the GUI and actually executes
+    # the finding of the hull
+    def compute_hull(self, points, pause, view):
+        self.pause = pause
+        self.view = view
+        assert (type(points) == list and type(points[0]) == QPointF)
 
+        t1 = time.time()
+        points = sorted(points, key=lambda point: point.x())
+        t2 = time.time()
+
+        t3 = time.time()
+        # this is a dummy polygon of the first 3 unsorted points
+        final_hull = self.divide_and_conquer(points)
+        polygon = []
+
+        # populate polygon list with values from linked list
+        linked_list_hull = final_hull.list
+        for val in linked_list_hull:
+            next_node = val.next
+            polygon.append(QLineF(val.value.x(), val.value.y(), next_node.value.x(), next_node.value.y()))
+
+        t4 = time.time()
+
+        # when passing lines to the display, pass a list of QLineF objects.  Each QLineF
+        # object can be created with two QPointF objects corresponding to the endpoints
+        self.showHull(polygon, RED)
+        self.showText('Time Elapsed (Convex Hull): {:3.3f} sec'.format(t4 - t3))
